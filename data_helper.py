9b10e8b553d120a2b1752375595184187d27ba01
dlcjfgmlnasa <dlcjfgmlnasa@naver.com>
2019-12-25 00:07:05 +0900

    Arxiv Paper 데이터셋 수집

--- data_helper.py
+++ data_helper.py
@@ -1,5 +1,6 @@
 # -*- coding:utf-8 -*-
 import os
+import random
 import argparse
 
 
@@ -10,9 +11,51 @@ def get_args():
     return parser.parse_args()
 
 
+BASE_DIR = './data'
+
+
 def collect_arxiv_paper_dataset():
     # Arxiv Paper 데이터셋 수집
     import arxiv
+    keywords = open(os.path.join(BASE_DIR, 'keyword'), 'r', encoding='utf-8').readlines()
+
+    # Collecting Dataset
+    total_content = []
+    for keyword in keywords:
+        keyword = keyword.strip()
+        temp = arxiv.query(query=keyword, max_results=10)
+        for t in temp:
+            title = ' '.join(t['title'].split())
+            summary = ' '.join(t['summary'].split())
+            line = title + '\t' + summary
+            total_content.append(line)
+
+    # Shuffling
+    random.shuffle(total_content)
+
+    # Split Train dataset / Test dataset / Validation dataset
+    train_ratio = 0.80
+    val_ratio = 0.10
+
+    total_size = len(total_content)
+    with open(os.path.join(BASE_DIR, 'train.dat'), 'a', encoding='utf-8') as f:
+        point = int(total_size * train_ratio)
+
+        for line in total_content[:point]:
+            f.write(line+'\n')
+
+    with open(os.path.join(BASE_DIR, 'val.dat'), 'a', encoding='utf-8') as f:
+        start_point = int(total_size * train_ratio)
+        end_point = int(total_size * (train_ratio + val_ratio))
+
+        for line in total_content[start_point:end_point]:
+            f.write(line + '\n')
+
+    with open(os.path.join(BASE_DIR, 'test.dat'), 'a', encoding='utf-8') as f:
+        point = int(total_size * (train_ratio + val_ratio))
+
+        for line in total_content[point:]:
+            f.write(line + '\n')
 
 
 if __name__ == '__main__':
